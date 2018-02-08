c6eaff71eb04d39a781443b0483e7a55bdaaa095
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2018-02-08 16:57:00 +0530

    if puweight 0, makeit 1

--- bbMET/bbMETBranchReader.py
+++ bbMET/bbMETBranchReader.py
@@ -2177,12 +2177,18 @@ def AnalyzeDataSet():
                 puweight = pileup2016histo.GetBinContent(pu_nTrueInt)
             else:
                 puweight = 1.
+                
+        
 #            print pu_nTrueInt
             
         #print (len_puweight, pu_nTrueInt, puweight)
         # ----------------------------------------------------------------------------------------------------------------------------------------------------------------
         ## Total weight
         # ----------------------------------------------------------------------------------------------------------------------------------------------------------------
+        if puweights == 0.0:
+            print 'Warning:: puweight is 0, setting it to 1'
+            puweights = 1.0
+
         muweights = muonTrig_SF * muIDSF_loose * muIDSF_tight * muIsoSF_loose * muIsoSF_tight * muTracking_SF
         if muweights == 0.0:
             print 'Warning:: muon weight is 0, setting it to 1'
