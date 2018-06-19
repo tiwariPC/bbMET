26a1dc417d18b057f0eebfb0f7662abf4bdfec93
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2018-06-19 20:52:45 +0530

    minor

--- bbMETplot/Scripts/syst/syst_plot.sh
+++ bbMETplot/Scripts/syst/syst_plot.sh
@@ -2,4 +2,4 @@ mkdir -p Systematics/ROOTFiles
 cd Systematics
 cp ../19062018/*syst*.root ./ROOTFiles
 cp ../19062018/*hadrecoil*.root ./ROOTFiles
-python plot_systematics.py
+python ../plot_systematics.py
