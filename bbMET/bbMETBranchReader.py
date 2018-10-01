e9a16f850cbd079df211c0347188578d75d6eeee
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2018-10-01 13:19:40 +0530

    minor comment change

--- bbMET/bbMETBranchReader.py
+++ bbMET/bbMETBranchReader.py
@@ -1970,7 +1970,7 @@ def AnalyzeDataSet():
                 if min( [DeltaPhi(WmunuPhi,myJetP4[nb].Phi()) for nb in range(nJets)] ) < 0.5: WdPhicond=False
 
         #1lep, 1 b-tagged
-        #if ((nEle==1 and nMu==0) or (nEle==0 and nMu==1)) and nTauTightElectron==0 and EleCRtrigstatus and WenuRecoil>200. and jetcond and Wenumass>50. and Wenumass<160. and pfMet > 50.:
+        #if nEle==1 and nMu==0 and nTauTightElectron==0 and EleCRtrigstatus and WenuRecoil>200. and jetcond and Wenumass>50. and Wenumass<160. and pfMet > 50.:
         if nEle==1 and nMu==0 and nTauTightElectron==0 and EleCRtrigstatus and WenuRecoil>200. and jetcond and Wenumass<160. and pfMet > 50.:
 
             iLeadLep=0
