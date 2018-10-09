50bb1e35ef12e97aab95f1e4dd48632e46cce51c
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2018-10-09 18:57:49 +0530

    fix

--- bbMETplot/Scripts/bbMET_StackFactory.py
+++ bbMETplot/Scripts/bbMET_StackFactory.py
@@ -364,7 +364,7 @@ for(int i =0; i<84; i++){
     fIn = new TFile(filenameString[i],"READ");
 
     //if(VARIABLEBINS){
-    if (varbin && histnameString.Index("hadrecoil") !=string::npos){
+    if (varbin && (histnameString.Index("hadrecoil") !=string::npos || histnameString.Index("met") !=string::npos)){
         std::cout << "Setting variable bining for recoil" << std::endl;
         h_temp = (TH1F*) fIn->Get(histnameString);
 
